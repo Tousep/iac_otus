2514ae3d4fee95b6145caa868b40413391254717
anekrasov <nekrasov.aa.w@gmail.com>
2023-09-24 12:23:25 +0300

    add auth using keyfile, cretaing network

---
+++ iac_lab2/terraform/.terraform.lock.hcl
@@ -0,0 +1,9 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/yandex-cloud/yandex" {
+  version = "0.99.0"
+  hashes = [
+    "h1:hDMdOacArHh49jksxuUnv/c9SowXM0cfE7HlT+4+Qxk=",
+  ]
+}
---
+++ iac_lab2/terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "yc_cloud" {
+  type = string
+  description = "Yandex Cloud ID"
+}
+
+variable "yc_folder" {
+  type = string
+  description = "Yandex Cloud folder"
+}
+
+variable "yc_key_file" {
+  type = string
+  description = "Yandex Cloud OAuth token"
+}
+
+variable "db_password" {
+  description = "MySQL user pasword"
+}
